8092071813fd4ff1a58a575c4892a3d607b9ce82
Sangeeta-kurmi <sangitakurmi320@gmail.com>
2025-06-22 20:30:51 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+#Task 1: Perform Basic Mathematical Operations
+a=input('a: ')
+b=input('b': ')
+p=int(b)
+n=int(a)
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Basic Mathematical Operations: ")
+print("Addition: {a} + {b} = {addition}")
+print("Subtraction: {a} - {b} = {subtraction}")
+print("Multiplication: {a} * {b} = {multiplication}")
+print("Division: {a} / {b} = {division}")
